c252fdd15896a8d703154f1d7d7060b0d50d4fc7
eduardofreitasf <eduardof.fernandes05@gmail.com>
2026-05-29 10:37:12 +0100

    add: game board component

---
+++ app/src/components/GameBoard.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { computed } from 'vue'
+import { useGameStore } from '../stores/game.js'
+import Pile from './Pile.vue'
+
+const store = useGameStore()
+
+const isSelectable = computed(() => store.phase === 'select')
+</script>
+
+<template>
+  <div class="game-board">
+    <Pile
+      v-for="pile in store.piles"
+      :key="pile.id"
+      :pile="pile"
+      :isSelected="store.selectedPileId === pile.id"
+      :isSelectable="isSelectable"
+    />
+  </div>
+</template>
+
+<style scoped>
+.game-board {
+  display: grid;
+  grid-template-columns: repeat(3, var(--card-w));
+  grid-template-rows: repeat(3, var(--card-h));
+  gap: 18px;
+}
+</style>
